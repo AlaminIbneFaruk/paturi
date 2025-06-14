64ee3b10d760ec1ca34daf50169f0048345f5f73
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-06-14 18:51:15 +0600

    fixing bug 05

--- src/app/Protected/page.js
+++
@@ -1,8 +0,0 @@
-import { auth } from '@/lib/auth';
-import { redirect } from 'next/navigation';
-
-export default async function ProtectedPage() {
-  const session = await auth();
-  if (!session) redirect('/auth/signin');
-  return <h1>Welcome, {session.user.name}</h1>;
-}
