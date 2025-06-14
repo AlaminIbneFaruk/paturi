f12b3182c59387182e66d7e5371aa60a9c3be41e
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-06-14 18:38:30 +0600

    fixing bug 02

--- src/app/actions/auth/RegisterUser.js
+++ src/app/actions/auth/RegisterUser.js
@@ -1,5 +1,5 @@
 "use server";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import DBConnect, { collectionNames } from "@/lib/dbConnect";
 
 export const registerUser = async (payload) => {
